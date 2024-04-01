fa5aa17b9c1ee701186dd595d8541dbc8608898f
Chen Prom <cprom@Chens-MBP.lan>
2024-03-31 21:20:00 -0500

    delete is working

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -32,7 +32,7 @@ export const  fetch = async (req, res) => {
 export const update = async (req, res) => {
     try{
         const id = req.params.id
-        const employeeExist = await Employee.findOne({_id: id})
+        const employeeExist = await Employee.findOne({_id:id})
 
         if(!employeeExist){
             return res.status(404).json({message: "Employee not found"})
@@ -44,3 +44,18 @@ export const update = async (req, res) => {
         res.status(500).json({error: "Internal Server Error"})
     }
 }
+
+// export const deleteEmployee = async (req, res) => {
+//     try{
+//         const id = req.params.id
+//         const employeeExist = await Employee.findOne({_id:id})
+
+//         if(!employeeExist){
+//             return res.status(404).json({message: "Employee not found"})
+//         }
+//         await Employee.findByIdAndDelete(id)
+//         res.status(201).json({message: "Employee terminated!!"})
+//     }catch(error){
+//         res.status(500).json({error: "Internal Server Error"})
+//     }
+// }
